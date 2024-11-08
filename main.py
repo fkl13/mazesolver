b1cc3e40e79876752163a411f89410474975f266
Fabian Klaiber <fklaiber@posteo.net>
2024-11-08 23:02:24 +0100

    Draw line

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from window import Window
+from point import Line
+from point import Point
 
 
 def main():
     win = Window(800, 600)
+    p1 = Point(2, 2)
+    p2 = Point(20, 23)
+    line = Line(p1, p2)
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -5,10 +5,10 @@ class Window:
     def __init__(self, width, height):
         self.root_widget = Tk()
         self.root_widget.title("Maze Solver")
+        self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.root_widget, bg="white", height=height, width=width)
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
-        self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.root_widget.update_idletasks()
@@ -21,3 +21,6 @@ class Window:
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
