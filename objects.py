e70719e7d13c67b091633f493a06f7d8706ed5ff
Fabian Klaiber <fklaiber@posteo.net>
2024-11-12 22:49:56 +0100

    Remove walls in maze

--- objects.py
+++ objects.py
@@ -26,6 +26,7 @@ class Cell:
         self._y1 = None
         self._y2 = None
         self._win = win
+        self.visited = False
 
     def draw(self, top_left_x, top_left_y, bottom_right_x, bottom_right_y):
         if self._win is None:
